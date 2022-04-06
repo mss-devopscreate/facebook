a073ff6c5fcd33468a71d97ef9abf8d95d271524
devopscreates <pushpa458707@gmail.com>
2022-04-06 20:12:15 +0530

    udated file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 udating in master branch..
+=======
+this file updating development branch..
+>>>>>>> development
 udateing this line
 this file contains the deployment script.
 update the file 
